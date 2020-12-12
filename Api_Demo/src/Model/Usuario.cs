f3d5abf045e3987ea8f438b379e65912650d320c
Anderson Silva <andersonsstr@gmail.com>
2020-12-11 22:47:24 -0300

    Adicionar arquivos de projeto.

---
+++ Api_Demo/src/Model/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Demo.src.Model
+{
+    public class Usuario
+    {
+        public string nome { get; set; }
+        public string idade { get; set; }
+
+        public Usuario(string nome, string idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public Usuario()
+        {
+        }
+    }
+}
---
+++ Api_Demo/src/Rotas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_Demo.src.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Demo.src
+{
+    [Produces("application/json")]
+    [ApiController]
+    public class Rotas : Controller
+    {
+        [HttpPost]
+        [Route("/teste")]
+        public Usuario receiver(object teste)
+        {
+            //dynamic json = new ExpandoObject();
+            //json.nome = "Anderson";
+            //json.idade = "20";
+
+            Usuario usuario = new Usuario("Anderson", "20");
+
+            return usuario;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public string receiver()
+        {
+
+            return "Hello World";
+        }
+    }
+}
